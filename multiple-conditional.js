d9362af98ad5ab8ea156761b15a1c78f4151f010
philiplbrown <philiplbrown022@gmail.com>
2024-02-18 13:19:33 +0600

    added multiple-conditional

---
+++ multiple-conditional.js
@@ -0,0 +1,53 @@
+// var isGrudation = true;
+// var salary = 75000;
+// var car = 1;
+
+// if (isGrudation == true){
+//     console.log("I'll marrey you.");
+// }
+// else{
+//     console.log("I won't marrey you.");
+// }
+// // ------------------------------------------
+// if (isGrudation !== true){
+//     console.log("I'll marrey you.");
+// }
+// else{
+//     console.log("I won't marrey you.");
+// }
+// // ==========================================
+
+// if (isGrudation == true && salary > 50000){
+//     console.log("I'll marrey you.");
+// }
+// else{
+//     console.log("I won't marrey you.");
+// }
+// // -------------------------------------------
+// if (isGrudation == true && salary < 50000){
+//     console.log("I'll marrey you.");
+// }
+// else{
+//     console.log("I won't marrey you.");
+// }
+
+// // ======================================================
+// if (isGrudation == true && salary > 50000 && car >= 1 ){
+//     console.log("I'll marrey you.");
+// }
+// else{
+//     console.log("I won't marrey you.");
+// }
+
+
+// -------------------------------------------------------
+var isGrudation = true;
+var salary = 31000;
+var car = 1;
+
+if (isGrudation === true || salary < 50000 || car >= 1){
+    console.log("I'll propose you");
+}
+else {
+    colsole.log('Give your friend number');
+}
