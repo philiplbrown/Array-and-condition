6ebd9a2918a45dbf1b1e5e5ee0a3c45201231342
philiplbrown <philiplbrown022@gmail.com>
2024-02-18 12:11:53 +0600

    added conditional

---
+++ conditional.js
@@ -0,0 +1,40 @@
+var pocox3 = 33500;
+var myBudget = 35500;
+if (pocox3 < myBudget){
+    console.log("I will buy the phone");
+}
+
+// ======================================
+var chickenPrice = 600;
+var myMoney = 250;
+// if (chickenPrice <= myMoney){
+//     console.log("I'll eat chicken");
+// }
+// if (chickenPrice > myMoney){
+//     console.log("I'll eat meshed");
+// }
+if (chickenPrice < myMoney){
+    console.log("I'll eat chicken");
+}
+else {
+    console.log("I'll eat potato")
+}
+// =================================
+
+var cowPrice = 1200000;
+var totalMoney = 90000;
+if ( cowPrice < totalMoney){
+    console.log("I'll buy the cow");
+}
+else {
+    console.log('I will buy a Goat');
+}
+// =================================
+var cowPrice = 1200000;
+var totalMoney = 90000;
+if ( cowPrice > totalMoney){
+    console.log("I'll buy the cow");
+}
+else {
+    console.log('I will buy a Goat');
+}
