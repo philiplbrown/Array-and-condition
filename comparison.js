6a0dd3db8abc110693df3ad4f056e63722a9e537
philiplbrown <philiplbrown022@gmail.com>
2024-02-18 11:37:38 +0600

    added comparison

---
+++ comparison.js
@@ -0,0 +1,23 @@
+// " < " means less then
+// " > " means geter then
+var number1 = 64;
+var number2 = 81;
+
+// Less then
+console.log( number1 < number2)
+// Geter then
+console.log( number1 > number2)
+// Equall equall
+console.log( number1 == number2)
+// Not equall
+console.log( number1 != number2)
+// Choto or Soman-kina
+console.log( number1 <= number2)
+// Boro or Somoan-kina
+console.log( number1 >= number2)
+
+
+// Multiple condition
+// money1 > money2 && number1 > number2 && player1 > player2
+
+// money1 > money2 || number1 > number2 || player1 > player2
