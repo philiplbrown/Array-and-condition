7f6495f2d88f8c7589c459c91391c7375346507b
philiplbrown <philiplbrown022@gmail.com>
2024-02-20 09:54:25 +0600

    added largest-problem-3, problem-2, problem-3

---
+++ problem-1.js
@@ -0,0 +1,18 @@
+var fruits =['Aopple', 'Banana', 'Orange'];
+console.log(fruits)
+
+// Find index
+var bananaIndex = fruits.indexOf('Banana');
+console.log(bananaIndex);
+
+// Replace Value
+fruits[1] = 'Mango';
+console.log(fruits)
+
+// Remove value
+fruits.pop("Orange");
+console.log(fruits)
+
+// Add a new value
+fruits.push("Watermelon");
+console.log(fruits)
---
+++ problem-2.js
@@ -0,0 +1,51 @@
+var sahinMark = 85;
+var tomMark = 66;
+var janeMark = 95;
+var peterMark = 66;
+var johnMark = 40;
+var A = 80;
+var B = 60;
+var C = 50;
+var D = 40;
+var F = 39;
+
+if(A <= sahinMark){
+    if(sahinMark >= A){
+        console.log('Sahin=A')
+    }
+    else{
+        console.log('Fail')
+    }
+}
+if(B <= tomMark){
+    if(tomMark >= B){
+        console.log('Tom=B')
+    }
+    else{
+        console.log('Fail')
+    }
+}
+if(A <= janeMark){
+    if(janeMark >= A){
+        console.log('Jane=A')
+    }
+    else{
+        console.log('Fail')
+    }
+}
+if(C <= peterMark){
+    if(peterMark >= C){
+        console.log('Peter=C')
+    }
+    else{
+        console.log('Fail')
+    }
+}
+if(D <= johnMark){
+    if(johnMark >= D){
+        console.log('John=D')
+    }
+    else{
+        console.log('Fail')
+    }
+}
